ca0530304ab2cd8d4ada2811fb1ff38d456611b1
Touré A. Karim <trabdlkarim@programmer.net>
2020-12-24 05:03:28 +0300

    Update custom.js

--- docs/_static/js/custom.js
+++ docs/_static/js/custom.js
@@ -6,10 +6,10 @@ var children = document.getElementsByTagName("FOOTER")[0].children;
 
 if(children.length > 0){
     for(let i=0; i < children.length; i++){
-      if(children[i].nodeName == "A"){
-          children[i].setAttribute("target","_blank");
-      }
+        if(children[i].nodeName == "A"){
+            children[i].setAttribute("target","_blank");
+        }
     }
-  }
+}
     
 });
